5dd33ff1cda870bd8730329747deb19a41291091
Bryan M <bmaitland101@gmail.com>
2022-12-16 08:07:49 -0600

    diable ci functions

--- R/10_prep.R
+++ R/10_prep.R
@@ -9,12 +9,12 @@ library(broom)
 
 
 # functions
-lower_ci <- function(mean, se, n, conf_level = 0.95){
-  lower_ci <- mean - qt(1-((1-conf_level)/2),n-1)*se
-}
-upper_ci <- function(mean, se, n, conf_level = 0.95){
-  upper_ci <- mean + qt(1-((1-conf_level)/2),n-1)*se
-}
+# lower_ci <- function(mean, se, n, conf_level = 0.95){
+#   lower_ci <- mean - qt(1-((1-conf_level)/2),n-1)*se
+# }
+# upper_ci <- function(mean, se, n, conf_level = 0.95){
+#   upper_ci <- mean + qt(1-((1-conf_level)/2),n-1)*se
+# }
 
 ## Data --------------------
 
