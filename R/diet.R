4591a7a2d1f07238d32d377ca83418beaa28e4cf
Bryan M <bmaitland101@gmail.com>
2022-12-16 12:28:57 -0600

    load and plot diet IRI data

---
+++ R/diet.R
@@ -0,0 +1,14 @@
+
+
+diet <- read_csv(here("data", "tmp_diet.csv"))
+
+
+diet |> 
+  left_join(PCAdat, by = "site_id") |> select(-PC2) |> 
+  ggplot(aes(PC1, IRI_100, color = food_group)) + 
+  geom_point() + 
+  geom_smooth(method = "lm", se = FALSE) + 
+  facet_wrap(vars(taxon_code)) + 
+  ylim(c(-10,100))
+
+
