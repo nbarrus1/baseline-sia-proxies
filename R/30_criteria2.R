d1030ff612fb4ccd8fb6442ca0f0611c62ad7d00
Nathan T. Barrus <nbarrus1@gmail.com>
2023-04-20 23:31:04 -0400

    synthesis table

--- R/30_criteria2.R
+++ R/30_criteria2.R
@@ -43,11 +43,14 @@ cv_ffg <- invertdata %>%
 ### Summary tables for CV data ------------
 
 cv_taxa |>
-  pivot_wider(names_from = "ffg", values_from = c("mean_d15n", "sd_d15n", "CV_d15n", "n"))
+  group_by(taxon_code) %>% 
+  summarise(CV = mean(CV_d15n),
+            SD = sd(CV_d15n))
 
 cv_ffg |>
-  pivot_wider(names_from = "ffg", values_from = c("mean_d15n", "sd_d15n", "CV_d15n", "n")) %>% 
-  filter(ffg != "Shredder")
+  group_by(ffg) %>% 
+  summarise(CV = mean(CV_d15n),
+            SD = sd(CV_d15n))
 
 
 ## Summarize mean CV  ---------
